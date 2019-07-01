2de185ab07fd19fe6db2fe9c9f987220245c07b4
JuliaShilo <akira677@mail.ru>
2019-07-01 20:14:58 +0300

    Edit tests

--- test/org/itstep/Service/FileManagerServiceTest.java
+++ test/org/itstep/Service/FileManagerServiceTest.java
@@ -11,87 +11,112 @@ import java.nio.file.Files;
 
 
 class FileManagerServiceTest {
-    private static String fromPath;
-    private static String toPath;
+    private static String jpgfromPath;
+    private static String jpgtoPath;
+    private static String txtFilefromPath;
+    private static String txtFiletoPath;
 
     @BeforeAll
     static void setPreData() {
         String MAIN_DIR = System.getProperty("user.dir");
         String SEPARATOR = System.getProperty("file.separator");
-        fromPath = MAIN_DIR + SEPARATOR + "files" + SEPARATOR + "Cat.jpg";
-        toPath = MAIN_DIR + SEPARATOR + "files" + SEPARATOR + "Cat_test_copy.jpg";
+
+        jpgfromPath = MAIN_DIR + SEPARATOR + "files" + SEPARATOR + "Cat.jpg";
+        jpgtoPath = MAIN_DIR + SEPARATOR + "files" + SEPARATOR + "Cat_test_copy.jpg";
+
+        txtFilefromPath = MAIN_DIR + SEPARATOR + "files" + SEPARATOR + "text.txt";
+        txtFiletoPath = MAIN_DIR + SEPARATOR + "files" + SEPARATOR + "text_test.txt";
     }
 
     @Test
     void testGetFileAsByteArray() {
-        byte[] bytes = FileManagerService.getFileAsByteArray(fromPath);
+        byte[] bytes = FileManagerService.getFileAsByteArray(jpgfromPath);
         assertNotNull(bytes);
         assertTrue(bytes.length > 0);
     }
 
     @Test
-    void writeByteArrayToFile() {
-        byte[] bytes = FileManagerService.getFileAsByteArray(fromPath);
+    void testWriteByteArrayToFile() {
+        byte[] bytes = FileManagerService.getFileAsByteArray(jpgfromPath);
 
-        FileManagerService.writeByteArrayToFile(bytes, toPath);
-        byte [] testBytes = FileManagerService.getFileAsByteArray(toPath);
+        FileManagerService.writeByteArrayToFile(bytes, jpgtoPath);
+        byte[] testBytes = FileManagerService.getFileAsByteArray(jpgtoPath);
 
-        assertNotNull (testBytes);
-        assertTrue (testBytes.length > 0);
+        assertNotNull(testBytes);
+        assertTrue(testBytes.length > 0);
+    }
+
+
+    @Test
+    void testGetTextFromFile() {
+        String text = FileManagerService.getTextFromFile(txtFilefromPath);
+        assertNotNull(text);
+        assertTrue(text.contains("someLine1\n"));
+        assertTrue(text.contains("someLine2\n"));
+        assertTrue(text.contains("Line3\n"));
+    }
+
+    @Test
+    void testWriteTexttoFileWithoutAppend() {
+        String text = "some test text\n" +
+                "need to be writen to file.";
+
+        FileManagerService.writeTexttoFile(txtFiletoPath, text, false);
+
+        String testText = FileManagerService.getTextFromFile(txtFiletoPath);
+
+        assertNotNull(testText);
+        assertEquals(testText, text + "\n");
+
+        FileManagerService.writeTexttoFile(txtFiletoPath, text, false);
+
+        testText = FileManagerService.getTextFromFile(txtFiletoPath);
+        assertNotNull(testText);
+        assertEquals(testText, text + "\n");
+    }
+
+    @Test
+    void testWriteTexttoFileWithAppend() {
+        String text = "some test text\n" +
+                "need to be writen to file.";
+
+        FileManagerService.writeTexttoFile(txtFiletoPath, text, false);
+
+        String testText = FileManagerService.getTextFromFile(txtFiletoPath);
+
+        assertNotNull(testText);
+        assertEquals(testText, text + "\n");
+
+        FileManagerService.writeTexttoFile(txtFiletoPath, text, false);
+
+        testText = FileManagerService.getTextFromFile(txtFiletoPath);
+        assertNotNull(testText);
+        assertEquals(testText, text + "\n" + text + "\n");
     }
 
 
     @AfterAll
     static void removeTestFiles() {
-        File file = new File(toPath);
-        if (file.isFile()) {
+        File jpgfile = new File(jpgtoPath);
+        File txtfile = new File(txtFiletoPath);
+
+        if (jpgfile.isFile()) {
             try {
-                Files.delete(file.toPath());
+                Files.delete(jpgfile.toPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+        if (txtfile.isFile()) {
+            try {
+                Files.delete(txtfile.toPath());
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
         }
     }
-
-
-
-    @BeforeAll
-    static void setPreData2() {
-        String MAIN_DIR = System.getProperty("user.dir");
-        String SEPARATOR = System.getProperty("file.separator");
-        fromPath = MAIN_DIR + SEPARATOR + "files" + SEPARATOR + "text.txt";
-        toPath = MAIN_DIR + SEPARATOR + "files" + SEPARATOR + "text_test_copy.txt";
-    }
-
-    @Test
-    void getTextFromFile () {
-        String text = FileManagerService.getTextFromFile(fromPath);
-        assertNotNull(text);
-    }
-
-    @Test
-    void writeTexttoFile () {
-        String text = FileManagerService.writeTexttoFile(fromPath);
-
-        FileManagerService.writeTexttoFile(text, toPath);
-        String testText = FileManagerService.writeTexttoFile(toPath);
-
-        assertNotNull (testText);
-
-    }
-
-    @AfterAll
-    static void removeTestFiles2() {
-        File file = new File(toPath);
-        if (file.isFile()) {
-            try {
-                Files.delete(file.toPath());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }
-    }
-
 }
